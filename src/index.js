34bfe196a13e8260fd67f4d2394e310a1a54160d
craexus <jjpeng@ryerson.ca>
2020-12-08 01:00:25 -0500

    initial component

--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyAbLWVAzrxJgTuWnTuwTt7fIPK59qkaMBM",
+  authDomain: "qnotes-5106e.firebaseapp.com",
+  databaseURL: "https://qnotes-5106e.firebaseio.com",
+  projectId: "qnotes-5106e",
+  storageBucket: "qnotes-5106e.appspot.com",
+  messagingSenderId: "921594280173",
+  appId: "1:921594280173:web:2bb3490717e6ec118974e4",
+  measurementId: "G-HJWV8XQSDN"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.storage();
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('qnote-container')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
